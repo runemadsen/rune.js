ca3c51356de985df9f5cd1b231d8520653573d86
Rune Madsen <rune@runemadsen.com>
2015-07-21 18:54:13 -0400

    more

--- test/spec/render.js
+++ test/spec/render.js
@@ -1,4 +1,4 @@
-describe("Render", function() {
+describe("SVG Render", function() {
 
   it("should create SVG element", function() {
     var r = new Rune({width:200, height:300});
@@ -7,5 +7,16 @@ describe("Render", function() {
     expect($(el).attr('width')).toEqual("200");
     expect($(el).attr('height')).toEqual("300");
   });
+
+  // all of these should have
+  // fill, stroke, strokewidth, strokeCap, position, rotation
+  it("should render rect");
+  it("should render ellipse");
+  it("should render circle");
+  it("should render line");
+  it("should render polygon");
+  it("should render path");
+
+  it("should render nested groups and shapes");
 
 });
