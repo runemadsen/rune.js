71f93b93e43f486f7fffdebcd8471510eacbd37e
Rune Madsen <rune@runemadsen.com>
2015-07-24 16:59:33 -0400

    all shapes

--- src/shapes/rect.js
+++ src/shapes/rect.js
@@ -12,11 +12,16 @@
     this.vars.height = height;
   };
 
-  _.extend(Rectangle.prototype,
-    Rune.Mixins.Moveable,
-    Rune.Mixins.Sizeable,
-    Rune.Mixins.Styleable,
-    { type: "rectangle"}
-  );
+  _.extend(Rectangle.prototype, Rune.Mixins.Shapeable, Rune.Mixins.Moveable, Rune.Mixins.Sizeable, Rune.Mixins.Styleable, {
+
+    type: "rectangle",
+
+    copy: function(group) {
+      var c = new Rune.Rectangle();
+      this.shapeCopy(c, group);
+      return c;
+    }
+
+  });
 
 })();
