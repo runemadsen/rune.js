c40f215e827ec010efc21508bb00b5130d3c2bba
Rune Madsen <rune@runemadsen.com>
2015-08-13 18:38:49 -0400

    deleting lib

--- browserify/virtual-dom.js
+++
@@ -1,6 +0,0 @@
-window.virtualdom = {};
-window.virtualdom.h = require('virtual-dom/h');
-window.virtualdom.diff = require('virtual-dom/diff');
-window.virtualdom.patch = require('virtual-dom/patch');
-window.virtualdom.createElement = require('virtual-dom/create-element');
-window.virtualdom.svg = require('virtual-dom/virtual-hyperscript/svg');
