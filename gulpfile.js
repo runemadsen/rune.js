36a0231454944346b481bff302071a7bc4fc91e0
Rune Madsen <rune@runemadsen.com>
2015-10-14 11:53:37 -0400

    browserify build works

--- gulpfile.js
+++ gulpfile.js
@@ -2,58 +2,42 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
+var babel = require('gulp-babel');
 var babelify = require('babelify');
 var rename = require("gulp-rename");
 var zip = require('gulp-zip');
 var uglify = require('gulp-uglify');
 var del = require('del');
-var watchify = require('watchify');
 var sourcemaps = require('gulp-sourcemaps');
 var assign = require('lodash.assign');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 var jasmine = require('gulp-jasmine');
 
-var builddir = './dist';
-
 // Build
 // -------------------------------------------------
 
-function compile(infiles, outfile, outdir,  extraOpts, watch) {
+function compile(infiles, outfile, outdir, extraOpts) {
 
-  var opts = assign({}, watchify.args, extraOpts);
-  var bundler = browserify(infiles, opts).transform(babelify.configure({sourceMaps:false}));
+  var opts = assign({}, extraOpts);
+  var bundler = browserify(infiles, opts)
+    .transform(babelify.configure({sourceMaps:false}));
 
-  //if(watch) {
-  //  bundler = watchify(bundler)
-  //}
-
-  function rebundle() {
-    return bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source(outfile))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(outdir));
-  }
-
-  //if(watch) {
-  //  bundler.on('update', function() {
-  //    console.log('-> bundling...');
-  //    rebundle();
-  //  });
-  //}
-
-  return rebundle();
+  return bundler.bundle()
+    .on('error', function(err) { console.error(err); this.emit('end'); })
+    .pipe(source(outfile))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(outdir));
 }
 
 gulp.task('build:browser', function() {
-  return compile('./src/rune.js', 'rune.browser.js', builddir, { debug:true }, false)
+  return compile('./src/rune.js', 'rune.browser.js', 'dist', { debug:true })
 });
 
-gulp.task('build:common', function() {
-  return compile('./src/rune.js', 'rune.common.js', builddir, { debug:true, bundleExternal:false }, false)
+gulp.task('build:npm', function() {
+  return compile('./src/rune.js', 'rune.common.js', 'dist', { bundleExternal:false, standalone: "default", debug:true })
 });
 
 gulp.task('build', ['build:common', 'build:browser']);
@@ -62,14 +46,14 @@ gulp.task('build', ['build:common', 'build:browser']);
 // -------------------------------------------------
 
 gulp.task('minify:clean', function (cb) {
-  del([builddir + '*min.js'], cb);
+  del(['dist/*min.js'], cb);
 });
 
 gulp.task('minify', ['build', 'minify:clean'], function() {
-  return gulp.src([builddir + '/*.js', '!' + builddir + '/*.min.js'])
+  return gulp.src(['dist/*.js', '!dist/*.min.js'])
     .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
-    .pipe(gulp.dest(builddir));
+    .pipe(gulp.dest('dist'));
 });
 
 // Release
@@ -77,20 +61,20 @@ gulp.task('minify', ['build', 'minify:clean'], function() {
 
 gulp.task('release', ['minify'], function() {
   var p = require('./package.json')
-  return gulp.src(builddir + '/*')
+  return gulp.src('dist/*')
     .pipe(zip('rune-'+p.version+'.zip'))
-    .pipe(gulp.dest(builddir));
+    .pipe(gulp.dest('dist'));
 });
 
 // Test
 // ---------------------------------------------------
 
 gulp.task('build:specs', function() {
-  return compile('test/specs_shared/index.js', 'shared.js', './test/lib', { debug:true }, false)
+  return compile('test/specs_shared/index.js', 'shared.js', './test/lib', { debug:true })
 });
 
 gulp.task('build:specs:browser', function() {
-  return compile('test/specs_browser/index.js', 'browser.js', './test/lib', { debug:true }, false)
+  return compile('test/specs_browser/index.js', 'browser.js', './test/lib', { debug:true })
 });
 
 gulp.task("test", ['build:browser', 'build:specs', 'build:specs:browser'], function() {
